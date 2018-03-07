e682971408b16a09207ac3af7911e34e296a6dd9
Landbergaren <Landbergaren@gmail.com>
2018-03-07 11:24:22 +0100

    Removing a bit of client info
    I guess the ip and stuff like that was not ment to be in the file :).. Heidisql beeing funny.

--- SQL/1.8.9/vehicle_groups.sql
+++ SQL/1.8.9/vehicle_groups.sql
@@ -1,10 +1,3 @@
--- --------------------------------------------------------
--- Host:                         108.174.57.160
--- Server version:               5.7.18-log - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             9.3.0.4984
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
